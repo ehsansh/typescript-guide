ee81324478aae183d1b17365a7d4487f631b59b5
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-08-06 16:31:27 +0330

    write a sorting class for strings

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,10 +1,19 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactorsCollection } from './CharactorsCollection';
 
-const numbersCollection = new NumbersCollection([9, 3, -1, 5, 6]);
+// const numbersCollection = new NumbersCollection([9, 35, -1, 5, 6]);
 
-const sorter = new Sorter(numbersCollection);
+// const sorter = new Sorter(numbersCollection);
+
+// sorter.sort();
+
+// console.log(numbersCollection.data);
+
+const charactersCollection = new CharactorsCollection('aksdjfqlqwenv');
+
+const sorter = new Sorter(charactersCollection);
 
 sorter.sort();
 
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
