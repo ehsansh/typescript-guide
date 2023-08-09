dff93c68f01279639be400e15a38bf0064d628bf
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-08-09 10:38:21 +0330

    change CsvFileReader to an abstract class and create matchreader class

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -2,11 +2,10 @@ import fs from 'fs';
 import { dateStringTodate } from './utils';
 import { MatchResult } from './MatchResult';
 
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-
-export class CsvFileReader {
-    data: MatchData[] = [];
+export abstract class CsvFileReader<T> {
+    data: T[] = [];
     constructor(public filename: string) {}
+    abstract mapRow(row: string[]): T;
     read(): void {
         this.data = fs
             .readFileSync(this.filename, {
@@ -16,16 +15,6 @@ export class CsvFileReader {
             .map((row: string): string[] => {
                 return row.split(',');
             })
-            .map((row: string[]): MatchData => {
-                return [
-                    dateStringTodate(row[0]),
-                    row[1],
-                    row[2],
-                    parseInt(row[3]),
-                    parseInt(row[4]),
-                    row[5] as MatchResult,
-                    row[6],
-                ];
-            });
+            .map(this.mapRow);
     }
 }
