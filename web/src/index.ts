9008562c1c94d0856ab020f3900602887e86d3b0
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-08-12 10:25:25 +0330

    add trigger method

--- web/src/index.ts
+++ web/src/index.ts
@@ -5,3 +5,12 @@ const user = new User({ name: 'ehsan', age: 40 });
 user.set({ name: 'asal' });
 
 console.log(user.get('name'));
+
+user.on('change', () => {
+    console.log('change 1');
+});
+user.on('change', () => {
+    console.log('change 2');
+});
+
+user.trigger('change');
