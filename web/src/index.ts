624669c415a3dacfc06ab0fdb41d811f88aeb09d
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-08-12 11:05:38 +0330

    insall axios and json web server and use them to get and updat data

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,16 +1,8 @@
-import { User } from './models/User';
+import axios from 'axios';
 
-const user = new User({ name: 'ehsan', age: 40 });
+// axios.post('http://localhost:3000/users', {
+//     name: 'ehsan',
+//     age: 40,
+// });
 
-user.set({ name: 'asal' });
-
-console.log(user.get('name'));
-
-user.on('change', () => {
-    console.log('change 1');
-});
-user.on('change', () => {
-    console.log('change 2');
-});
-
-user.trigger('change');
+axios.get('http://localhost:3000/users/1');
