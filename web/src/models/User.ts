3412313a95642e172f850f3ec8186ec2bcef13b6
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-08-12 12:44:33 +0330

    refactor with composition

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -6,10 +6,7 @@ interface UserProps {
     age?: number;
 }
 
-type Callback = () => void;
-
 export class User {
-    events: { [key: string]: Callback[] } = {};
     constructor(private data: UserProps) {}
     get(propName: string): number | string {
         return this.data[propName];
@@ -17,18 +14,7 @@ export class User {
     set(update: UserProps): void {
         Object.assign(this.data, update);
     }
-    on(eventName: string, callback: Callback) {
-        const handlers = this.events[eventName] || [];
-        handlers.push(callback);
-        this.events[eventName] = handlers;
-    }
-    trigger(eventName: string): void {
-        const handlers = this.events[eventName];
-        if (!handlers || handlers.length === 0) return;
-        handlers.forEach(callback => {
-            callback();
-        });
-    }
+
     fetch(): void {
         axios
             .get(`http://localhost:3000/users/${this.get('id')}`)
