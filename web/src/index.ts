b067040c37212bcad7c527c18ff89c5e2ebe6fba
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-08-13 15:32:45 +0330

    write save method

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,9 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
+const user = new User({ id: 1, name: 'james', age: 1 });
 
-user.on('change', () => {
-    console.log('something has changes');
+user.on('save', () => {
+    console.log(user);
 });
 
-user.fetch();
+user.save();
