c3ec3b331a9a0380214a836038447ae3cf846aaa
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-08-13 15:17:48 +0330

    write fetch method in User class

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,5 +1,9 @@
-// import { User } from './models/User';
+import { User } from './models/User';
 
-// const user = new User({ name: 'new record', age: 0 });
+const user = new User({ id: 1 });
 
-// user.save();
+user.on('change', () => {
+    console.log('something has changes');
+});
+
+user.fetch();
