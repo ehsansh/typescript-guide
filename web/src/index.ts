761d5a15f43554eb9c378ef8b549f6118e35c7ba
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-08-13 10:22:28 +0330

    creat Sync class and extract fetch and save method from User class

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,5 +1,5 @@
-import { User } from './models/User';
+// import { User } from './models/User';
 
-const user = new User({ name: 'new record', age: 0 });
+// const user = new User({ name: 'new record', age: 0 });
 
-user.save();
+// user.save();
